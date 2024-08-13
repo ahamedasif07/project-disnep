b59ea05f2d75ab687b0b5b869f8bb9a5d3f26c4e
rxasif07 <rxasif31@gmail.com>
2024-08-13 21:13:53 +0600

    secnd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Header from './Components/Header'
+import Movies from './Components/Movies'
 import Navbar from './Components/Navbar'
 
 function App() {
@@ -9,9 +10,10 @@ function App() {
   return (
     <>
 
-      <div className='text-gray-100'>
+      <div className='text-gray-100 bg-[#040714]'>
       <Navbar></Navbar>
       <Header></Header>
+      <Movies></Movies>
       </div>
  
     </>
---
+++ src/Components/Movies.jsx
@@ -0,0 +1,15 @@
+
+const Movies = () => {
+    return (
+        <div>
+            <div className="flex gap-4 justify-center">
+                <button className="hover:border-b-2 py-1">TRENDING</button>
+                <button className="hover:border-b-2 py-1">NEW ON DISNEY+</button>
+                <button className="hover:border-b-2 py-1">COMING SOON</button>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Movies;
